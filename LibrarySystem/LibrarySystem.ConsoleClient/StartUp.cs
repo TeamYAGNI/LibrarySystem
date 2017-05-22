04a5f39a63ca74dc9dd3828455817a7a2a28e0e6
ludzhev <ludzhev@gmail.com>
2017-05-22 19:20:59 +0300

    Add simple directory organization.

---
+++ LibrarySystem/LibrarySystem.ConsoleClient/StartUp.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="StartUP.cs" company="YAGNI">
+// All rights reserved.
+// </copyright>
+
+namespace LibrarySystem.ConsoleClient
+{
+    /// <summary>
+    /// Represent the Console Client of the Library System application starting point.
+    /// </summary>
+    public class StartUp
+    {
+        /// <summary>
+        /// Represent the main method called in the application live.
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
