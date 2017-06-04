87f9f639cf15b68c36507023c25f99aeffb196af
ludzhev <ludzhev@gmail.com>
2017-06-04 22:19:52 +0300

    Rename GenreTests and Remove duplicating configurations.

--- LibrarySystem/LibrarySystem.Models.UnitTests/Properties/AssemblyInfo.cs
+++ LibrarySystem/LibrarySystem.Models.UnitTests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4feae5be-ced1-4902-81fe-5ae11a51ca40")]
+[assembly: Guid("c720eeaf-ee42-43f9-9444-2ee407267519")]
 
 // Version information for an assembly consists of the following four values:
 //
