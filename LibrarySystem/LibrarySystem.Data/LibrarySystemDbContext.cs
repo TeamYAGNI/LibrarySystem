dcf6b61b6e7d3dd624d6ac708c902d52cb572a24
ludzhev <ludzhev@gmail.com>
2017-06-04 23:41:59 +0300

    Add connection strings for SQLServer and SQLExpress.

--- LibrarySystem/LibrarySystem.Data/LibrarySystemDbContext.cs
+++ LibrarySystem/LibrarySystem.Data/LibrarySystemDbContext.cs
@@ -1,3 +1,8 @@
+// <copyright file = "LibrarySystemDbContext.cs" company="YAGNI">
+// All rights reserved.
+// </copyright>
+// <summary>Holds implementation of LibrarySystemDbContext class.</summary>
+
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
@@ -5,44 +10,92 @@ using LibrarySystem.Models;
 
 namespace LibrarySystem.Data
 {
+    /// <summary>
+    /// Represent a <see cref="LibrarySystemDbContext"/> class, heir of <see cref="DbContext"/>.
+    /// </summary>
     public class LibrarySystemDbContext : DbContext
     {
-        // Your context has been configured to use a 'LibrarySystemDbContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'LibrarySystem.Data.LibrarySystemDbContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'LibrarySystemDbContext' 
-        // connection string in the application configuration file.
+        /// <summary>
+        /// Name of the connection string that describe witch database server <see cref="LibrarySystemDbContext"/> instance to use and how.
+        /// </summary>
+        //// private const string ConnectionString = "LibrarySystemSQLExpress";
+
+        /// <summary>
+        /// Name of the connection string that describe witch database server <see cref="LibrarySystemDbContext"/> instance to use and how.
+        /// </summary>
+        private const string ConnectionString = "LibrarySystemSQLServer";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LibrarySystemDbContext"/> class.
+        /// </summary>
         public LibrarySystemDbContext()
-            : base("name=LibrarySystemDbContext")
+            : base(ConnectionString)
         {
         }
 
-        //// Add a DbSet for each entity type that you want to include in your model. For more information 
-        //// on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
+        /// <summary>
+        /// Gets or sets access point to Addresses table in Database.
+        /// </summary>
         public virtual IDbSet<Address> Addresses { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Authors table in Database.
+        /// </summary>
         public virtual IDbSet<Author> Authors { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Books table in Database.
+        /// </summary>
         public virtual IDbSet<Book> Books { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Cities table in Database.
+        /// </summary>
         public virtual IDbSet<City> Cities { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Clients table in Database.
+        /// </summary>
         public virtual IDbSet<Client> Clients { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Employees table in Database.
+        /// </summary>
         public virtual IDbSet<Employee> Employees { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Genres table in Database.
+        /// </summary>
         public virtual IDbSet<Genre> Genres { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Journals table in Database.
+        /// </summary>
         public virtual IDbSet<Journal> Journals { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Lendings table in Database.
+        /// </summary>
         public virtual IDbSet<Lending> Lendings { get; set; }
-        
+
+        /// <summary>
+        /// Gets or sets access point to Publishers table in Database.
+        /// </summary>
         public virtual IDbSet<Publisher> Publishers { get; set; }
 
+        /// <summary>
+        /// Gets or sets access point to Subjects table in Database.
+        /// </summary>
         public virtual IDbSet<Subject> Subjects { get; set; }
 
+        /// <summary>
+        /// This method is called when the model for a derived context has been initialized,
+        /// but before the model has been locked down and used to initialize the context.
+        /// The default implementation of this method does nothing, but it can be overridden
+        /// in a derived class such that the model can be further configured before it is
+        /// locked down.
+        /// </summary>
+        /// <param name="modelBuilder">The builder that defines the model for the context being created.</param>
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             // Fluent API
