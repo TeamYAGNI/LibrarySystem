8fac94ebdf0320d90e5f713afcb6e8e77c1d1497
ludzhev <ludzhev@gmail.com>
2017-06-08 22:58:03 +0300

    Add CommandProcessor unit tests.

--- LibrarySystem/LibrarySystem.Framework.UnitTests/Providers/CommandProcessorTests/Constructor_Should.cs
+++ LibrarySystem/LibrarySystem.Framework.UnitTests/Providers/CommandProcessorTests/Constructor_Should.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
+using LibrarySystem.Commands.Contracts;
+using LibrarySystem.Framework.Providers;
+
+using Moq;
 using NUnit.Framework;
 
 namespace LibrarySystem.Framework.UnitTests.Providers.CommandProcessorTests
@@ -9,5 +11,26 @@ namespace LibrarySystem.Framework.UnitTests.Providers.CommandProcessorTests
     [TestFixture]
     public class Constructor_Should
     {
+        [Test]
+        [Category("Framework.Providers.CommandProcessor.Constructor")]
+        public void ThrowArgumentNullException_WhenNullPassedAsCommandsFactory()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => new CommandProcessor(null));
+        }
+
+        [Test]
+        [Category("Framework.Providers.CommandProcessor.Constructor")]
+        public void NotThrow_WhenValidCommandsFactoryPassed()
+        {
+            // Arrange
+            var commandsFactoryStub = new Mock<ICommandsFactory>();
+
+            // Act
+            // Assert
+            Assert.DoesNotThrow(() => new CommandProcessor(commandsFactoryStub.Object));
+        }
     }
 }
