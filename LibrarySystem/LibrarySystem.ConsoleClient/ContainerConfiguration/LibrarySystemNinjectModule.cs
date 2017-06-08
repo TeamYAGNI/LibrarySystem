a4a82eb748d25e73ad40745c44bac9a19f6082cc
ludzhev <ludzhev@gmail.com>
2017-06-08 06:00:03 +0300

    Add Ninject configuration.

---
+++ LibrarySystem/LibrarySystem.ConsoleClient/ContainerConfiguration/LibrarySystemNinjectModule.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file = "LibrarySystemNinjectModule.cs" company="YAGNI">
+// All rights reserved.
+// </copyright>
+// <summary>Holds implementation of LibrarySystemNinjectModule class.</summary>
+using System;
+using System.Linq;
+
+using LibrarySystem.Commands.Contracts;
+using LibrarySystem.ConsoleClient.ConsoleProviders;
+using LibrarySystem.Framework;
+using LibrarySystem.Framework.Contracts;
+using LibrarySystem.Framework.Providers;
+
+using Ninject.Activation;
+using Ninject.Extensions.Factory;
+using Ninject.Modules;
+
+namespace LibrarySystem.ConsoleClient.ContainerConfiguration
+{
+    /// <summary>
+    /// Represent a <see cref="LibrarySystemNinjectModule"/> class, heir of <see cref="NinjectModule"/> interface.
+    /// </summary>
+    public class LibrarySystemNinjectModule : NinjectModule
+    {
+        /// <summary>
+        /// Loads the module into the kernel.
+        /// </summary>
+        public override void Load()
+        {
+            // Basic Baindings
+            this.Bind<IEngine>().To<Engine>().InSingletonScope();
+
+            this.Bind<ICommandReader>().To<ConsoleCommandReader>().WhenInjectedExactlyInto<Engine>().InSingletonScope();
+            this.Bind<IResponseWriter>().To<ConsoleResponseWriter>().WhenInjectedExactlyInto<Engine>().InSingletonScope();
+            this.Bind<ICommandProcessor>().To<CommandProcessor>().WhenInjectedExactlyInto<Engine>().InSingletonScope();
+
+            // CommandFactory Bindings
+            this.Bind<ICommandsFactory>().ToFactory().WhenInjectedExactlyInto<ICommandProcessor>().InSingletonScope();
+            this.Bind<ICommand>().ToMethod(context => this.GetCommand(context))
+                .NamedLikeFactoryMethod((ICommandsFactory factory) => factory.GetCommand(null));
+
+            // Command Bindings
+
+            // Command Dependancies Bindings
+        }
+
+        /// <summary>
+        /// Represent the method witch provide a instance of <see cref="ICommand"/> implementator by a given command name in context argument.
+        /// </summary>
+        /// <param name="context">The context in witch the factory method have been invoked</param>
+        /// <returns>Instance of <see cref="ICommand"/> implementator</returns>
+        private ICommand GetCommand(IContext context)
+        {
+            string commandName = context.Parameters.Single().GetValue(context, null).ToString();
+
+            switch (commandName.ToLower())
+            {
+                case "commandNameToLower": // TODO: Add case for each command!
+                    // return context.Kernel.Get<ICommand>("commandName");
+                default:
+                    throw new InvalidOperationException(string.Format("There is no such command as {0}!", commandName)); // TODO: Implement InvalidCommandException;
+            }
+        }
+    }
+}
