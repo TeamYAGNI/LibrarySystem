de84d64287eab1c41b964c57572183a4da7ec0e5
ludzhev <ludzhev@gmail.com>
2017-06-08 05:50:51 +0300

    Add Console providers.

---
+++ LibrarySystem/LibrarySystem.ConsoleClient/ConsoleProviders/ConsoleCommandReader.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file = "ConsoleCommandReader.cs" company="YAGNI">
+// All rights reserved.
+// </copyright>
+// <summary>Holds implementation of ConsoleCommandReader class.</summary>
+
+using System;
+
+using LibrarySystem.Framework.Contracts;
+
+namespace LibrarySystem.ConsoleClient.ConsoleProviders
+{
+    /// <summary>
+    /// Represent a <see cref="ConsoleCommandReader"/> class, implementator of <see cref="ICommandReader"/> interface.
+    /// </summary>
+    public class ConsoleCommandReader : ICommandReader
+    {
+        /// <summary>
+        /// Represent the method witch provide a command.
+        /// </summary>
+        /// <returns>Command as string.</returns>
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
