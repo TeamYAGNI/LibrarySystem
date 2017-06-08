22a88458b1186e0450f1c48588124bb6939253d9
ludzhev <ludzhev@gmail.com>
2017-06-08 05:19:52 +0300

    Add Commands Contracts.

---
+++ LibrarySystem/LibrarySystem.Commands.Contracts/ICommandsFactory.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file = "ICommandsFactory.cs" company="YAGNI">
+// All rights reserved.
+// </copyright>
+// <summary>Holds constrains of ICommandsFactory implementators.</summary>
+
+namespace LibrarySystem.Commands.Contracts
+{
+    /// <summary>
+    /// Represent a <see cref="ICommandsFactory"/> interface.
+    /// </summary>
+    public interface ICommandsFactory
+    {
+        /// <summary>
+        /// Represent the method witch provide a instance of <see cref="ICommand"/> implementator by a given command name.
+        /// </summary>
+        /// <param name="commandName">Name of the command to be provided.</param>
+        /// <returns>Instance of <see cref="ICommand"/> implementator</returns>
+        ICommand GetCommand(string commandName);
+    }
+}
