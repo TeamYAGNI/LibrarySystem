28daa40e5affe6b5432064bf72879f7f155215be
ludzhev <ludzhev@gmail.com>
2017-06-09 00:13:37 +0300

    Add ModelValidation unit tests.

---
+++ LibrarySystem/LibrarySystem.ConsoleClient.UnitTests/Interceptors/ModelValidationTests/Constructor_Should.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using LibrarySystem.ConsoleClient.Interceptors;
+using LibrarySystem.Framework.Contracts;
+
+using Moq;
+using NUnit.Framework;
+
+namespace LibrarySystem.ConsoleClient.UnitTests.Interceptors.ModelValidationTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        [Category("ConsoleClient.Interceptors.ModelValidation.Constructor")]
+        public void ThrowArgumentNullException_WhenNullPassedAsValidator()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => new ModelValidation(null));
+        }
+
+        [Test]
+        [Category("ConsoleClient.Interceptors.ModelValidation.Constructor")]
+        public void NotThrow_WhenValidatorPassed()
+        {
+            // Arrange
+            var validatorStub = new Mock<IValidator>();
+
+            // Act
+            // Assert
+            Assert.DoesNotThrow(() => new ModelValidation(validatorStub.Object));
+        }
+    }
+}
---
+++ LibrarySystem/LibrarySystem.ConsoleClient.UnitTests/Interceptors/ModelValidationTests/Intercept_Should.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using LibrarySystem.ConsoleClient.Interceptors;
+using LibrarySystem.Framework.Contracts;
+
+using Moq;
+using Ninject.Extensions.Interception;
+using NUnit.Framework;
+
+namespace LibrarySystem.ConsoleClient.UnitTests.Interceptors.ModelValidationTests
+{
+    [TestFixture]
+    public class Intercept_Should
+    {
+        [Test]
+        [Category("ConsoleClient.Interceptors.ModelValidation.Intercept")]
+        public void CallInvocationProceedExactlyOnce_WhenValidInvocationPassed()
+        {
+            // Arrange
+            var validatorStub = new Mock<IValidator>();
+            var modelValidationUnderTest = new ModelValidation(validatorStub.Object);
+
+            var invocationMock = new Mock<IInvocation>();
+
+            // Act
+            modelValidationUnderTest.Intercept(invocationMock.Object);
+
+            // Assert
+            invocationMock.Verify(i => i.Proceed(), Times.Once);
+        }
+
+        [Test]
+        [Category("ConsoleClient.Interceptors.ModelValidation.Intercept")]
+        public void CallValidatorValidateExactlyOnceWithReturnValueOfInvocation_WhenValidArgumentsPassed()
+        {
+            // Arrange
+            var validatorMock = new Mock<IValidator>();
+            var modelValidationUnderTest = new ModelValidation(validatorMock.Object);
+
+            string returnValue = "GenericReturnValue";
+
+            var invocationStub = new Mock<IInvocation>();
+            invocationStub.SetupGet(i => i.ReturnValue).Returns(returnValue);
+
+            // Act
+            modelValidationUnderTest.Intercept(invocationStub.Object);
+
+            // Assert
+            validatorMock.Verify(v => v.Validate<object>(returnValue, It.IsAny<Action<string>>()), Times.Once);
+        }
+    }
+}
