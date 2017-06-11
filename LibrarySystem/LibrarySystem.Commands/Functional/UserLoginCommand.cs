7e8c23b6c365ecf20813814d5bd4d2a4d18029c3
ArnaudovSt <telerikacademy2016@gmail.com>
2017-06-12 02:19:28 +0300

    User login/logout command

---
+++ LibrarySystem/LibrarySystem.Commands/Functional/UserLoginCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySystem.Commands.Contracts;
+using LibrarySystem.Repositories.Data.Users.Contracts;
+
+namespace LibrarySystem.Commands.Functional
+{
+    public class UserLoginCommand : ICommand
+    {
+        private readonly IUserRepository usersRepository;
+
+        public UserLoginCommand(IUserRepository usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+
+        public string Execute(IList<string> parameters)
+        {
+            var username = parameters[0];
+            var password = parameters[1];
+
+            var hasLogged = this.usersRepository.LoginUser(username, password);
+
+            if (!hasLogged)
+            {
+                return $"User {username} authentication failed.";
+            }
+
+            return $"User {username} has been logged in.";
+        }
+    }
+}
---
+++ LibrarySystem/LibrarySystem.Commands/Functional/UserLogoutCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySystem.Commands.Contracts;
+using LibrarySystem.Repositories.Data.Users.Contracts;
+
+namespace LibrarySystem.Commands.Functional
+{
+    public class UserLogoutCommand : ICommand
+    {
+        private readonly IUserRepository usersRepository;
+
+        public UserLogoutCommand(IUserRepository usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+
+        public string Execute(IList<string> parameters)
+        {
+            var username = parameters[0];
+            var password = parameters[1];
+
+            var hasLoggedOut = this.usersRepository.LogoutUser(username, password);
+
+            if (!hasLoggedOut)
+            {
+                return $"User {username} authentication failed.";
+            }
+
+            return $"User {username} successfully logged out.";
+        }
+    }
+}
