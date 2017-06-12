5622097bf9a6de03d21a165c65aa478e0d675772
ludzhev <ludzhev@gmail.com>
2017-06-12 23:10:40 +0300

    Configure implemented commands bindings.

--- LibrarySystem/LibrarySystem.ConsoleClient/ContainerConfiguration/LibrarySystemNinjectModule.cs
+++ LibrarySystem/LibrarySystem.ConsoleClient/ContainerConfiguration/LibrarySystemNinjectModule.cs
@@ -2,10 +2,20 @@
 // All rights reserved.
 // </copyright>
 // <summary>Holds implementation of LibrarySystemNinjectModule class.</summary>
-using System;
+
 using System.Linq;
 
+using LibrarySystem.Commands.Administrative.Listings.Client;
+using LibrarySystem.Commands.Administrative.Listings.Employee;
+using LibrarySystem.Commands.Administrative.Listings.User;
 using LibrarySystem.Commands.Contracts;
+using LibrarySystem.Commands.Functional;
+using LibrarySystem.Commands.Projection.Authors;
+using LibrarySystem.Commands.Projection.Books;
+using LibrarySystem.Commands.Projection.Genres;
+using LibrarySystem.Commands.Projection.Journals;
+using LibrarySystem.Commands.Projection.Publishers;
+using LibrarySystem.Commands.Projection.Subjects;
 using LibrarySystem.ConsoleClient.Interceptors;
 using LibrarySystem.ConsoleClient.LocalProviders;
 using LibrarySystem.Framework;
@@ -14,6 +24,7 @@ using LibrarySystem.Framework.Exceptions;
 using LibrarySystem.Framework.Providers;
 using LibrarySystem.Models.Factory;
 
+using Ninject;
 using Ninject.Activation;
 using Ninject.Extensions.Factory;
 using Ninject.Extensions.Interception.Infrastructure.Language;
@@ -26,6 +37,37 @@ namespace LibrarySystem.ConsoleClient.ContainerConfiguration
     /// </summary>
     public class LibrarySystemNinjectModule : NinjectModule
     {
+        private const string GetAllClientsWithDelayedLendingsCommand = "GetAllClientsWithDelayedLendings";
+        private const string GetAllClientsWithJournalsCommand = "GetAllClientsWithJournals";
+        private const string GetClientAddressByPINCommand = "GetClientAddressByPIN";
+        private const string GetClientByPINCommand = "GetClientByPIN";
+        private const string GetLendingsByClientPINCommand = "GetLendingsByClientPIN";
+        private const string GetRemarksByClientPINCommand = "GetRemarksByClientPIN";
+        private const string GetAllEmployeesByManagerNameCommand = "GetAllEmployeesByManagerName";
+        private const string GetAllEmployeesWithoutManagerCommand = "GetAllEmployeesWithoutManager";
+        private const string GetEmployeesByFullNameCommand = "GetEmployeesByFullName";
+        private const string GetLoggedInUsersCommand = "GetLoggedInUsers";
+        private const string ClientExitLibraryCommand = "ClientExitLibrary";
+        private const string ClientGetBookCommand = "ClientGetBook";
+        private const string ClientGetJournalCommand = "ClientGetJournal";
+        private const string ClientReturnBookCommand = "ClientReturnBook";
+        private const string ClientReturnJournalsCommand = "ClientReturnJournals";
+        private const string UserLoginCommand = "UserLogin";
+        private const string UserLogoutCommand = "UserLogout";
+        private const string GetAuthorsByBookTitleCommand = "GetAuthorsByBookTitle";
+        private const string GetBookByISBNCommand = "GetBookByISBN";
+        private const string GetBooksByAuthorCommand = "GetBooksByAuthor";
+        private const string GetBooksByGenreNameCommand = "GetBooksByGenreName";
+        private const string GetBooksByPublisherCommand = "GetBooksByPublisher";
+        private const string GetGenresWithMostBooksCommand = "GetGenresWithMostBooks";
+        private const string GetJournalByISSNCommand = "GetJournalByISSN";
+        private const string GetJournalsByPublisherNameCommand = "GetJournalsByPublisherName";
+        private const string GetJournalsBySubjectCommand = "GetJournalsBySubject";
+        private const string GetPublisherByBookTitleCommand = "GetPublisherByBookTitle";
+        private const string GetPublisherByJournalTitleCommand = "GetPublisherByJournalTitle";
+        private const string GetSubjectsWithHighestImpactFactorcs = "GetSubjectsWithHighestImpactFactorcs";
+        private const string GetSubjectsWithMostJournalsCommand = "GetSubjectsWithMostJournals";
+
         /// <summary>
         /// Loads the module into the kernel.
         /// </summary>
@@ -44,6 +86,36 @@ namespace LibrarySystem.ConsoleClient.ContainerConfiguration
                 .NamedLikeFactoryMethod((ICommandsFactory factory) => factory.GetCommand(null));
 
             // Command Bindings
+            this.Bind<ICommand>().To<GetAllClientsWithDelayedLendingsCommand>().Named(GetAllClientsWithDelayedLendingsCommand);
+            this.Bind<ICommand>().To<GetAllClientsWithJournalsCommand>().Named(GetAllClientsWithJournalsCommand);
+            this.Bind<ICommand>().To<GetClientAddressByPINCommand>().Named(GetClientAddressByPINCommand);
+            this.Bind<ICommand>().To<GetClientByPINCommand>().Named(GetClientByPINCommand);
+            this.Bind<ICommand>().To<GetLendingsByClientPINCommand>().Named(GetLendingsByClientPINCommand);
+            this.Bind<ICommand>().To<GetRemarksByClientPINCommand>().Named(GetRemarksByClientPINCommand);
+            this.Bind<ICommand>().To<GetAllEmployeesByManagerNameCommand>().Named(GetAllEmployeesByManagerNameCommand);
+            this.Bind<ICommand>().To<GetAllEmployeesWithoutManagerCommand>().Named(GetAllEmployeesWithoutManagerCommand);
+            this.Bind<ICommand>().To<GetEmployeesByFullNameCommand>().Named(GetEmployeesByFullNameCommand);
+            this.Bind<ICommand>().To<GetLoggedInUsersCommand>().Named(GetLoggedInUsersCommand);
+            this.Bind<ICommand>().To<ClientExitLibraryCommand>().Named(ClientExitLibraryCommand);
+            this.Bind<ICommand>().To<ClientGetBookCommand>().Named(ClientGetBookCommand);
+            this.Bind<ICommand>().To<ClientGetJournalCommand>().Named(ClientGetJournalCommand);
+            this.Bind<ICommand>().To<ClientReturnBookCommand>().Named(ClientReturnBookCommand);
+            this.Bind<ICommand>().To<ClientReturnJournalsCommand>().Named(ClientReturnJournalsCommand);
+            this.Bind<ICommand>().To<UserLoginCommand>().Named(UserLoginCommand);
+            this.Bind<ICommand>().To<UserLogoutCommand>().Named(UserLogoutCommand);
+            this.Bind<ICommand>().To<GetAuthorsByBookTitleCommand>().Named(GetAuthorsByBookTitleCommand);
+            this.Bind<ICommand>().To<GetBookByISBNCommand>().Named(GetBookByISBNCommand);
+            this.Bind<ICommand>().To<GetBooksByAuthorCommand>().Named(GetBooksByAuthorCommand);
+            this.Bind<ICommand>().To<GetBooksByGenreNameCommand>().Named(GetBooksByGenreNameCommand);
+            this.Bind<ICommand>().To<GetBooksByPublisherCommand>().Named(GetBooksByPublisherCommand);
+            this.Bind<ICommand>().To<GetGenresWithMostBooksCommand>().Named(GetGenresWithMostBooksCommand);
+            this.Bind<ICommand>().To<GetJournalByISSNCommand>().Named(GetJournalByISSNCommand);
+            this.Bind<ICommand>().To<GetJournalsByPublisherNameCommand>().Named(GetJournalsByPublisherNameCommand);
+            this.Bind<ICommand>().To<GetJournalsBySubjectCommand>().Named(GetJournalsBySubjectCommand);
+            this.Bind<ICommand>().To<GetPublisherByBookTitleCommand>().Named(GetPublisherByBookTitleCommand);
+            this.Bind<ICommand>().To<GetPublisherByJournalTitleCommand>().Named(GetPublisherByJournalTitleCommand);
+            this.Bind<ICommand>().To<GetSubjectsWithHighestImpactFactorcs>().Named(GetSubjectsWithHighestImpactFactorcs);
+            this.Bind<ICommand>().To<GetSubjectsWithMostJournalsCommand>().Named(GetSubjectsWithMostJournalsCommand);
 
             // Command Dependancies Bindings
             this.Bind<IModelsFactory>().To<ModelsFactory>().WhenInjectedInto<ICommand>().InSingletonScope().Intercept().With<ModelValidation>();
@@ -61,10 +133,37 @@ namespace LibrarySystem.ConsoleClient.ContainerConfiguration
 
             switch (commandName.ToLower())
             {
-                case "commandNameToLower": // TODO: Add case for each command!
-                                           // return context.Kernel.Get<ICommand>("commandName");
-                default:
-                    throw new InvalidCommandException(commandName);
+                case GetAllClientsWithDelayedLendingsCommand: return context.Kernel.Get<ICommand>(GetAllClientsWithDelayedLendingsCommand);
+                case GetAllClientsWithJournalsCommand: return context.Kernel.Get<ICommand>(GetAllClientsWithJournalsCommand);
+                case GetClientAddressByPINCommand: return context.Kernel.Get<ICommand>(GetClientAddressByPINCommand);
+                case GetClientByPINCommand: return context.Kernel.Get<ICommand>(GetClientByPINCommand);
+                case GetLendingsByClientPINCommand: return context.Kernel.Get<ICommand>(GetLendingsByClientPINCommand);
+                case GetRemarksByClientPINCommand: return context.Kernel.Get<ICommand>(GetRemarksByClientPINCommand);
+                case GetAllEmployeesByManagerNameCommand: return context.Kernel.Get<ICommand>(GetAllEmployeesByManagerNameCommand);
+                case GetAllEmployeesWithoutManagerCommand: return context.Kernel.Get<ICommand>(GetAllEmployeesWithoutManagerCommand);
+                case GetEmployeesByFullNameCommand: return context.Kernel.Get<ICommand>(GetEmployeesByFullNameCommand);
+                case GetLoggedInUsersCommand: return context.Kernel.Get<ICommand>(GetLoggedInUsersCommand);
+                case ClientExitLibraryCommand: return context.Kernel.Get<ICommand>(ClientExitLibraryCommand);
+                case ClientGetBookCommand: return context.Kernel.Get<ICommand>(ClientGetBookCommand);
+                case ClientGetJournalCommand: return context.Kernel.Get<ICommand>(ClientGetJournalCommand);
+                case ClientReturnBookCommand: return context.Kernel.Get<ICommand>(ClientReturnBookCommand);
+                case ClientReturnJournalsCommand: return context.Kernel.Get<ICommand>(ClientReturnJournalsCommand);
+                case UserLoginCommand: return context.Kernel.Get<ICommand>(UserLoginCommand);
+                case UserLogoutCommand: return context.Kernel.Get<ICommand>(UserLogoutCommand);
+                case GetAuthorsByBookTitleCommand: return context.Kernel.Get<ICommand>(GetAuthorsByBookTitleCommand);
+                case GetBookByISBNCommand: return context.Kernel.Get<ICommand>(GetBookByISBNCommand);
+                case GetBooksByAuthorCommand: return context.Kernel.Get<ICommand>(GetBooksByAuthorCommand);
+                case GetBooksByGenreNameCommand: return context.Kernel.Get<ICommand>(GetBooksByGenreNameCommand);
+                case GetBooksByPublisherCommand: return context.Kernel.Get<ICommand>(GetBooksByPublisherCommand);
+                case GetGenresWithMostBooksCommand: return context.Kernel.Get<ICommand>(GetGenresWithMostBooksCommand);
+                case GetJournalByISSNCommand: return context.Kernel.Get<ICommand>(GetJournalByISSNCommand);
+                case GetJournalsByPublisherNameCommand: return context.Kernel.Get<ICommand>(GetJournalsByPublisherNameCommand);
+                case GetJournalsBySubjectCommand: return context.Kernel.Get<ICommand>(GetJournalsBySubjectCommand);
+                case GetPublisherByBookTitleCommand: return context.Kernel.Get<ICommand>(GetPublisherByBookTitleCommand);
+                case GetPublisherByJournalTitleCommand: return context.Kernel.Get<ICommand>(GetPublisherByJournalTitleCommand);
+                case GetSubjectsWithHighestImpactFactorcs: return context.Kernel.Get<ICommand>(GetSubjectsWithHighestImpactFactorcs);
+                case GetSubjectsWithMostJournalsCommand: return context.Kernel.Get<ICommand>(GetSubjectsWithMostJournalsCommand);
+                default: throw new InvalidCommandException(commandName);
             }
         }
     }
